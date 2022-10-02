efb0d751f154da9e1a9ea5c6dcdfa02418733c09
jason <jason@jasonparker.info>
2022-10-02 01:11:42 -0400

    initial commit

--- basics/main.go
+++ basics/main.go
@@ -3,62 +3,93 @@ package main
 import (
 	"fmt"
 	"strings"
+	"basics/helper"
 )
 
-func main() {
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets uint = 50
-	
-	fmt.Printf("Welcome to %v booking application\n", conferenceName)
-	fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
-	fmt.Print("Get your tickets here to attend\n")
+var conferenceName = "Go Conference"
+const conferenceTickets = 50
+var remainingTickets uint = 50
+var bookings = []string{}
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
-	
-	var bookings []string
+func main() {
+
+	greetUsers()
 
 	for {
-		fmt.Println("Enter your first name: ")
-		fmt.Scan(&firstName)
-		
-		fmt.Println("Enter your last name: ")
-		fmt.Scan(&lastName)
-		
-		fmt.Println("Enter your email: ")
-		fmt.Scan(&email)
+		firstName, lastName, email, userTickets := getUserInput()
 
-		fmt.Println("Enter the number of tickets: ")	
-		fmt.Scan(&userTickets)
+		isValidName, isValidEmail, isValidTicketNumber := helper.ValidateUserInput(firstName, lastName, email, userTickets, remainingTickets)
 
-		isValidName := len(firstName) >= 2 &&len(lastName) >= 2
-		isValidEmail := strings.Contains(email, "@")
-		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+		if  isValidName && isValidEmail && isValidTicketNumber {
+			
+			bookTicket(userTickets, firstName, lastName, email)
 
-		if isValidName && isValidEmail && isValidTicketNumber {
-			remainingTickets = remainingTickets - userTickets
-			bookings = append(bookings, firstName + " " + lastName)
-	
-			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-			fmt.Printf("%v ticket remaining for %v\n", remainingTickets, conferenceName)
-	
-			firstNames := []string{}
-			for _,booking := range bookings {
-				var names = strings.Fields(booking)
-				firstNames = append(firstNames, names[0])
-			}
-	
+			var firstNames = getFirstNames()
 			fmt.Printf("The first name of bookings are: %v\n", firstNames)
+
 	
 			if remainingTickets == 0 {
 				fmt.Println("Our conference is booked out. Come back next year.")
 				break
 			}
 		} else {
-			fmt.Printf("Your input data is invalid, please try again!")
+
+			if !isValidName {
+				fmt.Println("First name is to short!")
+			}
+
+			if !isValidEmail {
+				fmt.Println("Email is invalid!")
+			}
+			
+			if !isValidTicketNumber {
+				fmt.Println("Number of tickets you entered is invalid!")
+			}
 		}
 	}
+}
+
+func greetUsers() {
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	fmt.Print("Get your tickets here to attend\n")
+}
+
+func getFirstNames() []string {
+	firstNames := []string{}
+	for _,booking := range bookings {
+		var names = strings.Fields(booking)
+		firstNames = append(firstNames, names[0])
+	}
+	return firstNames
+}
+
+func getUserInput() (string, string, string, uint) {
+	
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+	
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+	
+	fmt.Println("Enter your email: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter the number of tickets: ")	
+	fmt.Scan(&userTickets)
+
+	return firstName, lastName, email, userTickets
+}
+
+func bookTicket(userTickets uint, firstName string, lastName string, email string) {
+	remainingTickets = remainingTickets - userTickets
+	bookings = append(bookings, firstName + " " + lastName)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v ticket remaining for %v\n", remainingTickets, conferenceName)
 }
